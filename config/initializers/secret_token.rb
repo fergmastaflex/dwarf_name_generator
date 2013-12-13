6c90a6dd5f4bc475aefa14a0de5ab596d86fff39
ferg <joey@fergmedia.com>
2013-12-13 14:00:48 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DwarfNameGenerator::Application.config.secret_token = '5720e10c387940058c06a43bff8a11108da02c242380786e9a7a4900d24ec01406670c6ab9d87ffa284a6ab03b3eec26a71839c7b0ebb663b2a49ba40c3e3b30'
